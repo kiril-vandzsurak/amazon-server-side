30d7892fad0014d3d6cbadd9d94ddd5bb3d4f795
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-12-23 16:28:36 +0100

    some updates

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -2,19 +2,19 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import fs from "fs-extra";
 
-const { readJSON, writeJSON, writeFile } = fs;
+const { writeFile } = fs;
 
-const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
-const productsJSONPath = join(dataFolderPath, "products.json");
-const reviewsJSONPath = join(dataFolderPath, "reviews.json");
+// const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
+// const productsJSONPath = join(dataFolderPath, "products.json");
+// const reviewsJSONPath = join(dataFolderPath, "reviews.json");
 const publicFolderPath = join(process.cwd(), "./public/images");
 
-export const getProducts = () => readJSON(productsJSONPath);
-export const writeProducts = (productsArray) =>
-  writeJSON(productsJSONPath, productsArray);
-export const getReviews = () => readJSON(reviewsJSONPath);
-export const writeReviews = (reviewArray) =>
-  writeJSON(reviewsJSONPath, reviewArray);
+// export const getProducts = () => readJSON(productsJSONPath);
+// export const writeProducts = (productsArray) =>
+//   writeJSON(productsJSONPath, productsArray);
+// export const getReviews = () => readJSON(reviewsJSONPath);
+// export const writeReviews = (reviewArray) =>
+//   writeJSON(reviewsJSONPath, reviewArray);
 
 export const saveProductImage = (fileName, contentAsBuffer) =>
   writeFile(join(publicFolderPath, fileName), contentAsBuffer);
